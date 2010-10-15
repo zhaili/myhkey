660ed40d3f957b1cb68d910294978897a6d8fad8
Zhai Li <mrzhaili@gmail.com>
2010-10-15 15:07:35 +0800

    Add util.cpp: New function `util::RunProcess'

---
+++ util.cpp
@@ -0,0 +1,41 @@
+#include "stdafx.h"
+#include "resource.h"
+
+#include <string.h>
+
+#include "util.h"
+
+namespace util {
+
+void RunProcess(LPCTSTR cmdline)
+{
+    TCHAR command[MAX_PATH] = {0};
+    strcpy(command, cmdline);
+
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    if (!CreateProcess(
+        NULL,
+        command,
+        NULL,
+        NULL,
+        FALSE,
+        0,
+        NULL,
+        NULL,
+        &si,
+        &pi))
+    {
+        // Failed
+        return;
+    }
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+}
+
+}
---
+++ util.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace util {
+
+void RunProcess(LPCTSTR cmdline);
+
+}
